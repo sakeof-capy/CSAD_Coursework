91c5f0f7473fe169d030259ebd37d963e69cfe2f
Fourier2718281828 <non-linear@ukr.net>
2023-06-29 00:42:52 +0300

    -redundant GetGoodQuantityOperation +all operation types

--- src/main/java/org/example/storage/operations/factory/OperationType.java
+++ src/main/java/org/example/storage/operations/factory/OperationType.java
@@ -2,7 +2,16 @@ package org.example.storage.operations.factory;
 
 import org.example.storage.Storage;
 import org.example.storage.operations.StorageOperation;
-import org.example.storage.operations.GetGoodQuantityOperation;
+import org.example.storage.operations.category.CreateCategoryOperation;
+import org.example.storage.operations.category.DeleteCategoryOperation;
+import org.example.storage.operations.category.ReadCategoryOperation;
+import org.example.storage.operations.category.UpdateCategoryOperation;
+import org.example.storage.operations.product.CreateProductOperation;
+import org.example.storage.operations.product.DeleteProductOperation;
+import org.example.storage.operations.product.ReadProductOperation;
+import org.example.storage.operations.product.UpdateProductOperation;
+import org.example.storage.operations.user.CreateUserOperation;
+import org.example.storage.operations.user.ReadUserOperation;
 
 import java.util.function.Function;
 
@@ -15,7 +24,18 @@ import java.util.function.Function;
  * to the value by passing its 'creator' as a param.
  */
 public enum OperationType {
-    GET_GOOD_QUANTITY(GetGoodQuantityOperation::new),
+    CREATE_PRODUCT(CreateProductOperation::new),
+    READ_PRODUCT(ReadProductOperation::new),
+    UPDATE_PRODUCT(UpdateProductOperation::new),
+    DELETE_PRODUCT(DeleteProductOperation::new),
+
+    CREATE_CATEGORY(CreateCategoryOperation::new),
+    READ_CATEGORY(ReadCategoryOperation::new),
+    UPDATE_CATEGORY(UpdateCategoryOperation::new),
+    DELETE_CATEGORY(DeleteCategoryOperation::new),
+
+    CREATE_USER(CreateUserOperation::new),
+    READ_USER(ReadUserOperation::new)
     ;
 
     OperationType(Function<Storage, StorageOperation> operation) {
